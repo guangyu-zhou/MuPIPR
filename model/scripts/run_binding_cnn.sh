663da4b898d28df7b5de506f8e62513ca6b50296
sebastien <sebastien@mims.ai>
2021-03-29 10:44:18 -0400

    trying to get an example to work?

--- model/scripts/run_binding_cnn.sh
+++ model/scripts/run_binding_cnn.sh
@@ -1,1 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python MuPIPR_CNN.py ../../data/binding_affinity/bpx.3g.txt ../../data/binding_affinity/bpx.seq.txt 4 results/bfx_3G_CNN_2l64_50_50_64.txt 50 50 64 -1
+
+echo '../../data/binding_affinity/bpx.3g.txt DOES NOT EXIST, PLEASE CHECK'
+
+#CUDA_VISIBLE_DEVICES=0 python MuPIPR_CNN.py ../../data/binding_affinity/bpx.3g.txt ../../data/binding_affinity/bpx.seq.txt 4 results/bfx_3G_CNN_2l64_50_50_64.txt 50 50 64 -1
--- model/scripts/run_binding_main.sh
+++ model/scripts/run_binding_main.sh
@@ -1,1 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 python MuPIPR.py ../../data/binding_affinity/bpx.3g.txt ../../data/binding_affinity/bpx.seq.txt 4 results/bfx_3G_2l64_50_50_64.txt 50 50 64 -1
+
+
+echo '../../data/binding_affinity/bpx.3g.txt DOES NOT EXIST, PLEASE CHECK'
+
+
+#CUDA_VISIBLE_DEVICES=0 python MuPIPR.py ../../data/binding_affinity/bpx.3g.txt ../../data/binding_affinity/bpx.seq.txt 4 results/bfx_3G_2l64_50_50_64.txt 50 50 64 -1
